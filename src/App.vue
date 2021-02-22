a886c82995d149bd0e9a725ae86470b0064ad3b9
N1Anoobis <mrbond84@hotmail.com>
2021-02-22 09:13:51 +0100

    Add player and monster attack methods

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,18 @@
 <template>
   <div id="app">
-    <header>
-      <NavBar />
-    </header>
+    <Game />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import NavBar from "./components/NavBar.vue";
+import Game from "./components/Game.vue";
 @Component({
   components: {
-    NavBar,
+    Game,
   },
 })
 export default class App extends Vue {}
 </script>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body {
-  font-family: ubuntu, sans-serif;
-  height: 100vh;
-  background-color: #bbb;
-}
-
-header {
-  width: 100vw;
-  background-color: #222;
-  padding: 15px;
-}
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from "./store";
-// import router from "./router";
+
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
-  // router,
-  render: (h) => h(App),
+  render: function(createElement) {
+    return createElement(App);
+  },
 }).$mount("#app");
